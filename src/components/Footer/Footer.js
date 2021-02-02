6a741035421df4e6cb1d11c81b08dd2caf3c27eb
Justin <pierrelampre@gmail.com>
2021-02-02 05:57:44 -0500

    realized the component structure needed to be reorganized to achieve the reponsive flow I was going for.  Also wrote first round of media queries.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import hire_me from "./img/hireme.gif"
 import "./footer.css";
 
 const Footer = () => {
     return (
         <div className="footer-container">
-            <div className="gif"></div>
+            <img src={hire_me} alt="A gif begging someone to hire me."/>
         </div>
     )
 }
--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import Header from "./Header/Header"
-import Info from "./Info/Info"
+import TV_G from "./img/tvg-logo.png"
+import simmons from "./img/simmons.gif"
 import "./info-panel.css"
 
 const InfoPanel = () => {
     return (
         <div className="info-panel-container">
-            <Header />
-            <Info />
+            <div className="logo"><h1>Tubecast</h1></div>
+            <div className="clock-box">
+                <p>TV Listings</p>
+                <div className="clock-and-mail">
+                    <p>*</p>
+                    <p>4:35am</p>
+                </div>
+            </div>
+            <div className="tv-guide">
+                <img src={TV_G} alt="" className="src" />
+            </div>
+            <div className="channel-and-program">
+                <div className="info-program">Richard Simmons: Dancin' To Manson</div>
+                <div className="info-channel">4 ION</div>
+            </div>
+            <div className="commercial">
+                <img src={simmons} alt="Richard Simmons dancing" />
+            </div>
+            <div className="blurb-box">
+                We're still not sure if we like the drugs, or if they like us.
+                But that won't stop us from dancin' off those pounds...
+            </div>
         </div>
     )
 }
